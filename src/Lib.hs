e50a2e209829b904e3147d90879352adef8a4fc1
Marvin Moosbacher <mmoosbacher@inovex.de>
2020-06-02 09:35:32 +0200

    Imported random-package + added test function for random number

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+    randomFunc
     ) where
+
+import System.Random        
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+randomFunc = do
+    gen <- getStdGen
+    print $"Zufaellige Zahl: " ++ show (head (randomRs (500,10000) gen) :: Int)
