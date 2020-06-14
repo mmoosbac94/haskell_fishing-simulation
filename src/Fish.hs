5eabf2df39a9c935fe68e7c06e8875860905eb4d
Marvin Moosbacher <mmoosbacher@inovex.de>
2020-06-14 12:58:03 +0200

    Implemented working function for randomLength for generatedFish

--- src/Fish.hs
+++ src/Fish.hs
@@ -18,7 +18,17 @@ generateRandomWeight = do
     gen <- newStdGen
     return (head (randomRs (500,10000) gen) :: Int)
 
+generateRandomLength :: IO Int
+generateRandomLength = do
+    gen <- newStdGen
+    return (head (randomRs (3,100) gen) :: Int)
+
+--generateRandomFishName :: IO String
+--generateRandomFishName = do
+        
+
 generateFish :: IO Fish
 generateFish = do
     weight <- generateRandomWeight
-    return Fish {fishName ="Goldfisch", fishLength=10, fishWeight=weight}
+    length <- generateRandomLength
+    return Fish {fishName ="Goldfisch", fishLength=length, fishWeight=weight}
