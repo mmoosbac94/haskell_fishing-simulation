7ae7e756afaaa2cecd935fb16c49c6a08c19737f
Thomas <tbaur1@smail.uni-koeln.de>
2020-06-24 15:32:24 +0200

    cleanup, random weather condition, query for weather condition, set Location

--- src/Game.hs
+++ src/Game.hs
@@ -3,14 +3,22 @@ module Game (startGame, startFishing) where
 import Text.Read
 import Data.Maybe
 import Angler
+import Weather
+import System.Exit
 
 angeln = "Angeln" 
+
 
 startGame = do
     putStrLn "Herzlich Willkommen zur Angelsimulation!"
     putStrLn $"Tippe '" ++ angeln ++ "' ein, wenn du bereit bist zu angeln."
     input <- getLine
     checkForValidInput input
+    currentWeather <- generateRandomWeatherCondition    
+    showBestFishingSpot currentWeather
+    if currentWeather == Hagel 
+        then exitSuccess  -- Programm an dieser Stelle beenden
+    else putStrLn $" gehen wir " ++ setLocation currentWeather ++ " angeln ..."
 
 createAngler = do
     putStrLn "Alles klar, wir wüssten natürlich gerne noch mit wem wir es heute zu tun haben!"
@@ -23,7 +31,7 @@ createAngler = do
     let angler = Angler {name = anglerName, age = anglerAge}
     putStrLn $"Vielen Dank! Du heisst also " ++ name angler ++ " und bist " ++ show(age angler) ++ " Jahre alt."
 
-startFishing = do
+startFishing = do    
     putStrLn "Lass uns die Angel auswerfen..."
     putStrLn "(Delay)"
     putStrLn "...Nanu, es hat etwas angebissen!... Es ist ein ..."
@@ -45,3 +53,23 @@ checkForValidAge anglerAge
         putStrLn "Butter bei die Fische, gib ein valides Alter ein..."
         input <- getLine
         checkForValidAge input
+
+
+showBestFishingSpot :: WeatherConditions -> IO()
+showBestFishingSpot weatherCondString
+    | weatherCondString == Sonnig = putStrLn "Super! Es ist sonnig, wir gehen ans Meer zum fischen!"
+    | weatherCondString == Wolkig = putStrLn "Das Wetter ist nicht so gut, lass uns lieber an den nächsten See gehen!"
+    | weatherCondString == Neblig = putStrLn "Es ist neblig. Du könntest die Fische nicht so gut sehen! Gehen wir an den Rhein!"
+    | weatherCondString == Regnerisch =  putStrLn "Es regnet! Wir fahren besser nach Norwegen und fischen an einem Fjord. Da gibt es bestimmt frischen Lachs!"
+    | weatherCondString == Gewitter = putStrLn "Pfui! Es sind starke Gewitter unterwegs. Wir gehen lieber zum Baggersee"
+    | weatherCondString == Schneefall = putStrLn "Es schneit! Eisfischen in Sibiren ist auch ganz cool!"
+    | weatherCondString == Hagel = putStrLn "Willst du wirklich Fischen gehen bei Hagel? Lass es besser!"
+
+setLocation :: WeatherConditions -> String
+setLocation Sonnig = "ans Meer"  
+setLocation Wolkig = "zum See"  
+setLocation Neblig = "zum Fluss" 
+setLocation Regnerisch = "zum Fjord in Norwegen" 
+setLocation Gewitter = "zum Baggersee"
+setLocation Schneefall = "nach Sibirien"
+setLocation Hagel = ""
--- src/Weather.hs
+++ src/Weather.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+
 module Weather where
 
 import Data.Aeson
@@ -8,65 +9,34 @@ import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
 import GHC.Generics
 
+import System.Random
+
 
 data Weather = Weather {
-    main :: String,
-    description :: String,
-    wind :: Int,
-    temperature :: Float
+    main :: String,     
+    temperature :: Int
 } deriving (Show, Eq, Generic)
 
--- instance FromJSON Weather
 
-instance FromJSON Weather where
- parseJSON (Object v) =
-    Weather <$> v .: "main"
-           <*> v .: "description"
-           <*> v .: "wind"
-           <*> v .: "temperature"
+data WeatherConditions = Sonnig | Wolkig | Neblig | Regnerisch | Gewitter | Schneefall | Hagel deriving (Show, Eq)
+weather = [Sonnig,Wolkig,Neblig,Regnerisch,Gewitter,Schneefall,Hagel]
 
-instance ToJSON Weather where
- toJSON (Weather main description wind temperature) =
-    object [ "main"         .= main
-           , "description"  .= description
-           , "wind"         .= wind
-           , "temperature"  .= temperature
-             ]
+generateRandomTemp :: IO Int
+generateRandomTemp = do
+    gen <- newStdGen
+    return (head (randomRs (-20,40) gen) :: Int)
 
 
-apiKey = "441e81fb7b249e1a876e04a163f30716"
+generateRandomWeatherCondition :: IO WeatherConditions
+generateRandomWeatherCondition = do 
+    gen <- newStdGen
+    let number = head (randomRs (0,length weather - 1) gen) :: Int
+    return $weather!!number
 
-stadt = "cologne"
-
-
-jsonURL :: String
-jsonURL = "http://api.openweathermap.org/data/2.5/weather?q=" ++ stadt ++ "&appid=" ++ apiKey
-
-getJSON :: IO B.ByteString
-getJSON = simpleHttp jsonURL
-
-convertData :: IO()
-convertData = do
-    -- get JSON data and encode it
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Weather])
-    -- If d is Left, the JSON was malformed.
-    -- In that case, we report the error.
-    -- Otherwise, we perform the operation of
-    -- our choice. In this case, just print it. 
-    case d of
-        Left err -> putStrLn err
-        Right w -> print w 
-
-{-
-jsonConverter :: IO()
-jsonConverter = do
-    input <- getJSON
-    let mm = decode input :: Maybe Weather
-    case mm of
-        Nothing -> print "Error in parsing JSON"
-        Just  -> decode input
--}
--- showingWeather w = (show.name) w ++ " , " ++ (show.temperature) w
-
-
-
+generateWeather :: IO Weather
+generateWeather = do
+    wname <- generateRandomWeatherCondition
+    let weatherCondString = show wname
+    temperature <- generateRandomTemp    
+    return Weather {main=weatherCondString, temperature=temperature}
+    
